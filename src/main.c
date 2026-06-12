534b8d9af71d2e7b059e748209474c19a12776f9
Sagar_T_V <tvsagar404@gmail.com>
2026-06-12 14:41:21 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 
 int main() {
     printf("Mul: %d\n", mul(2, 3));
-    printf("Div: %d\n", div(10, 2));
+    printf("Div: %d\n", divide(10, 2));
     printf("Sum: %d\n", add_results(1,2,3,4));
     return 0;
 }
