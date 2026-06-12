7df796c15ecca555cd97f35d4d1924fe3d03772f
Sagar_T_V <tvsagar404@gmail.com>
2026-06-12 14:34:08 +0530

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "calculator.h"
+#include "integration.h"
+#include <stdio.h>
+
+int main() {
+    printf("Mul: %d\n", mul(2, 3));
+    printf("Div: %d\n", div(10, 2));
+    printf("Sum: %d\n", add_results(1,2,3,4));
+    return 0;
+}
